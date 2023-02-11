ca39d13b064b6ad457a91b1a98c8d2f552596360
Isabelle Henson <hensonir@mail.uc.edu>
2023-02-11 00:05:14 -0500

    Lab 04 01

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Directions:
-        /*
-        Given the price of a purchase the program computes and outputs a 5% sales tax.
-        Be sure to use output statements that clearly display the price and the computed sales tax.
-        (No conditions here, the sales tax is always the same rate.)
-         */
-
         final double SALES_TAX_PERCENT = 0.05; // 5% tax used to compute itemTax
         double itemPrice;     // for base price of item (user input)
         double itemTax;       // stores the tax to be placed on the item
